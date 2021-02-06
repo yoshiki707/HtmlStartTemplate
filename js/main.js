6cb82891be4cbfae270268e53e542ad09f4d624a
Yoshiki Murakami <veb.brang@gmail.com>
2021-02-06 10:55:05 +0900

    jsの追加

--- js/main.js
+++ js/main.js
@@ -5,3 +5,15 @@ $(function () {
     $("#js-menu-button").toggleClass("menu-button-open");
   });
 });
+// スムーススクロール
+$(function () {
+  $('a[href^="#"]').click(function () {
+		var speed = 800;
+		var headerH = $('#Header').height();
+		var href = $(this).attr("href");
+		var target = $(href == "#" || href == "" ? 'html' : href);
+		var position = target.offset().top - headerH - 10;
+		$('body,html').animate({ scrollTop: position }, speed, 'swing');
+		return false;
+	});
+})
